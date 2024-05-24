26442883c59b3c8f1b8bfc978289883ae51f1e2b
akai <2780896955@qq.com>
2024-05-24 09:42:24 +0800

    README

--- main.py
+++ main.py
@@ -39,26 +39,26 @@ def crawl(urls_dir: Path, output_dir: Path):
         crawler.crawl()
 
 def split_content(content_dirs: Path, word_count_limit=1000, word_count_thres=40):
-    
     for content_dir in content_dirs:
         for content_file in content_dir.iterdir():
             if content_file.is_file():
                 split_file(content_file, word_count_limit=word_count_limit, word_count_thres=word_count_thres)
 
 if __name__ == '__main__':
-    # urls_dir = Path('crawler_data/rought_urls')
-    # base_url = 'https://minecraft.wiki'
-    # urls = [
-    #     'https://minecraft.wiki/w/Mob',
-    #     'https://minecraft.wiki/w/Block',
-    #     'https://minecraft.wiki/w/Item',
-    #     'https://minecraft.wiki/w/Tutorials',
-    #     'https://minecraft.wiki/w/Biome',
-    #     'https://minecraft.wiki/w/Smithing',
-    #     'https://minecraft.wiki/w/Structure'
-    # ]
-    # url_crawl(base_url=base_url, urls=urls, output_dir=urls_dir, rough=True)
-    # crawl(urls_dir=urls_dir, output_dir=Path('crawler_data/rough') )
+    urls_dir = Path('crawler_data/rought_urls')
+    base_url = 'https://minecraft.wiki'
+    urls = [
+        'https://minecraft.wiki/w/Mob',
+        'https://minecraft.wiki/w/Block',
+        'https://minecraft.wiki/w/Item',
+        'https://minecraft.wiki/w/Tutorials',
+        'https://minecraft.wiki/w/Biome',
+        'https://minecraft.wiki/w/Smithing',
+        'https://minecraft.wiki/w/Structure'
+    ]
+    url_crawl(base_url=base_url, urls=urls, output_dir=urls_dir, rough=True)
+    crawl(urls_dir=urls_dir, output_dir=Path('crawler_data/rough'))
+    
     content_dirs = [
         Path(path) for path in Path('crawler_data/rough').iterdir() if path.is_dir()
     ]
